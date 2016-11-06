ac75a486f51da4045f03f80d89d279d1395794f7
jhomble <jhomble7@gmail.com>
2016-11-05 23:30:21 -0400

    Added test chart

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,6 +5,33 @@ angular.module('app.controllers', [])
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams, buildingService, $ionicModal, $q, $rootScope, $state) {
 
+    $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+    $scope.series = ['Series A', 'Series B'];
+    $scope.data = [
+        [65, 59, 80, 81, 56, 55, 40],
+        [28, 48, 40, 19, 86, 27, 90]
+    ];
+
+    $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+    $scope.options = {
+        scales: {
+        yAxes: [
+            {
+            id: 'y-axis-1',
+            type: 'linear',
+            display: true,
+            position: 'left'
+            },
+            {
+            id: 'y-axis-2',
+            type: 'linear',
+            display: true,
+            position: 'right'
+            }
+        ]
+        }
+    };
+
     $scope.$on("$ionicView.beforeEnter", function(event, data){
         $scope.getImage = function(image){
             var promise = buildingService.getURL(image);
@@ -88,6 +115,33 @@ function ($scope, $stateParams, buildingService, $ionicModal, $q, $rootScope, $s
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams, $ionicUser, $firebaseAuth, $state, userService, campaignService) {
     
+    $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+    $scope.series = ['Series A', 'Series B'];
+    $scope.data = [
+        [65, 59, 80, 81, 56, 55, 40],
+        [28, 48, 40, 19, 86, 27, 90]
+    ];
+
+    $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+    $scope.options = {
+        scales: {
+        yAxes: [
+            {
+            id: 'y-axis-1',
+            type: 'linear',
+            display: true,
+            position: 'left'
+            },
+            {
+            id: 'y-axis-2',
+            type: 'linear',
+            display: true,
+            position: 'right'
+            }
+        ]
+        }
+    };
+
     $scope.$on("$ionicView.beforeEnter", function(event, data){
         // handle event
         console.log("State Params: ", data.stateParams);
