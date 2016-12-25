43587f31eb17014a678c7f803e107a1168234339
jhomble <jhomble7@gmail.com>
2016-12-25 18:23:31 -0500

    added import

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -901,10 +901,10 @@ function ($scope, $stateParams, campaignService, $firebaseAuth, userService) {
     updateMessages()
 }])
 
-.controller('campaignPicturesCtrl', ['$scope', '$stateParams', 'campaignService', 'userService', '$ionicModal', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('campaignPicturesCtrl', ['$scope', '$stateParams', 'campaignService', 'userService', '$ionicModal', '$cordovaCamera', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams, campaignService, userService, $ionicModal) {
+function ($scope, $stateParams, campaignService, userService, $ionicModal, $cordovaCamera) {
     
     $scope.$on("$ionicView.beforeEnter", function(event, data){
 
